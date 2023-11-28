92b94f92977605ce5c8c91b67ef38d30fb54ed4b
jhusep <silva.velez.es@gmail.com>
2023-11-28 09:41:16 +0100

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async(category) => {
+
+   const url = `http://api.giphy.com/v1/gifs/search?api_key=LlLdC8fCnu198OKtprkTEupTmiUmlD8b&q=${category}&limit=10`;
+   const resp = await fetch( url );
+   const { data } = await resp.json();
+ 
+   const gifs = data.map( img => ({
+     id: img.id,
+     title: img.title,
+     url: img.images.downsized_medium.url
+   }));
+ 
+   return gifs;
+ };
+
