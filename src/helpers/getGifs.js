6388ce5d4a1d67afa3116cd70d1c72af1372e96c
jhusep <silva.velez.es@gmail.com>
2023-11-28 11:33:54 +0100

    se corrigieron errores

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async(category) => {
 
-   const url = `http://api.giphy.com/v1/gifs/search?api_key=LlLdC8fCnu198OKtprkTEupTmiUmlD8b&q=${category}&limit=10`;
+   const url = `https://api.giphy.com/v1/gifs/search?api_key=LlLdC8fCnu198OKtprkTEupTmiUmlD8b&q=${category}&limit=10`;
    const resp = await fetch( url );
    const { data } = await resp.json();
  
